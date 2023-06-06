426dfeaf8f7b6fb2122ada62040b3ed1e59a2fb2
sayra <sayra.bano8888@gmail.com>
2023-06-06 12:06:44 +0530

    'changesdone'

--- config/mongoose.js
+++ config/mongoose.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 // mongoose.connect('mongodb://localhost/codeial_development');
 
 // connecting to databse on mongodb cloud
-mongoose.connect('mongodb+srv://sayrabano8888:lZwnVwnXe7OrlRN5@cluster0.ebxvdye.mongodb.net/?retryWrites=true&w=majority',{
+mongoose.connect('mongodb+srv://sayrabano8888:mdOZ2NqcyHBeFGBe@cluster0.zwczm6d.mongodb.net/?retryWrites=true&w=majority',{
     useNewUrlParser:true
 })
 
